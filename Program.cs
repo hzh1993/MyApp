553fefd342be1a1e5261e55d6fd771ad35888e19
hzh <huzhenhua1993@outlook.com>
2025-04-29 09:09:01 +0000

    第一次提交

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//	sudo dotnet new console -n MyApp 添加项目
+//  sudo dotnet build MyApp.csproj   编译
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Worldsd!");
